3792e4367d51aabd81c5970adf9b2ebd65155e62
MenukaRanasinghe <hgmranasinghe@gmail.com>
2023-11-27 02:45:04 +0530

    login validated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,5 @@
+// login.component.ts
+
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import AuthService from 'src/app/service/auth-service/auth-service.service';
@@ -7,30 +9,56 @@ import AuthService from 'src/app/service/auth-service/auth-service.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-
 export class LoginComponent {
-
   loginEmail: string = '';
   loginPassword: string = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  emailError: string = '';
+  passwordError: string = '';
+  serverError: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
   onLoginHandler = () => {
+    // Reset error messages
+    this.emailError = '';
+    this.passwordError = '';
+    this.serverError = '';
+
+    // Validation checks
+    if (!this.loginEmail) {
+      this.emailError = 'Email is required';
+    }
+
+    if (!this.loginPassword) {
+      this.passwordError = 'Password is required';
+    }
+
+    // If any validation error exists, stop further processing
+    if (this.emailError || this.passwordError) {
+      return;
+    }
+
+    // Continue with login logic
     const _data = {
       'email': this.loginEmail.trim().toLowerCase(),
       'password': this.loginPassword.trim()
-    }
+    };
+
+    // Provide success and error callbacks
     this.authService.newUserLogin(
       _data,
       this.onSuccessHandler,
       this.onErrorHandler
-    ).subscribe()
-  }
+    ).subscribe();
+  };
 
   onSuccessHandler = (response: any) => {
-    this.router.navigate(['/home'], { queryParams: { success: true } })
-  }
+    this.router.navigate(['/home'], { queryParams: { success: true } });
+  };
 
-  onErrorHandler = (e: any) => {
-    console.log(e)
-  }
+  onErrorHandler = (error: any) => {
+    // Handle server error or specific error messages
+    this.serverError = 'Incorrect email or password';
+  };
 }

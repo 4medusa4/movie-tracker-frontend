131075b391afc8b0be576cb84293cdf2bad93d4c
Medusa <imeshalakshani007@gmail.com>
2023-11-27 07:43:30 +0530

    navbar done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,20 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import AuthService from './service/auth-service/auth-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe((loggedIn: boolean) => {
+      this.isLoggedIn = loggedIn;
+    });  }
   title = 'movie-tracker-frontend';
-  // navbg: any;
-
-  // @HostListener('document:scroll') scrollover(){
-  //   console.log(document.body.scrollTop,'scrolllength#');
-
-  //   if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
-  //     this.navbg = {
-  //       'background-color': '#ffffff'
-  //     }
-  //   }else{
-  //     this.navbg = {}
-  //   }
-  // }
 }

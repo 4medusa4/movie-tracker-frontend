38bd7c2ad77152410a11e67a6b059fe1cc96eb22
Kraken <warunamadushanka456@gmail.com>
2023-12-16 21:58:16 +0530

    tracklist saving method added

--- src/app/service/movie-api-service/movie-api-service.service.ts
+++ src/app/service/movie-api-service/movie-api-service.service.ts
@@ -11,7 +11,6 @@ export class MovieApiServiceService {
   constructor(private http: HttpClient) { }
 
   tmdb_base_url = "https://api.themoviedb.org/3";
-  // apikey = "d3084da6f3cd42ca9f3a122b1d6d146a";
 
   baseurl = "http://localhost:8080/api/v1";
   apikey = "d3084da6f3cd42ca9f3a122b1d6d146a";
@@ -219,131 +218,6 @@ export class MovieApiServiceService {
     return this.http.post(`${this.baseurl}/tmdb/movies`, payload);
   }
 
-
-
-
-
-
-
-  // trendingMovieApiData(): Observable<any> { //new release movies watch in sri lanka
-
-  //   const payload = {
-  //     "airDateGte": null,
-  //     "airDateLte": null,
-  //     "certification": null,
-  //     "certificationCountry": "",
-  //     "debug": false,
-  //     "firstAirDateGte": null,
-  //     "firstAirDateLte": null,
-  //     "page": 1,
-  //     "primaryReleaseDateGte": null,
-  //     "primaryReleaseDateLte": null,
-  //     "region": "US",
-  //     "releaseDateGte": "",
-  //     "releaseDateLte": "",
-  //     "showMe": 0,
-  //     "sortBy": "popularity",
-  //     "voteAverageGte": 0.0,
-  //     "voteAverageLte": 10.0,
-  //     "voteCountGte": 0,
-  //     "watchRegion": "LK",
-  //     "withGenres": "",
-  //     "withKeywords": null,
-  //     "withNetworks": null,
-  //     "withOriginCountry": null,
-  //     "withOriginalLanguage": "en",
-  //     "withWatchMonetizationTypes": null,
-  //     "withWatchProviders": null,
-  //     "withReleaseType": "",
-  //     "withRuntimeGte": 0,
-  //     "withRuntimeLte": 400
-  //   };
-
-  //   return this.http.post(`${this.baseurl}/tmdb/movies`, payload);
-  // }
-
-  // upcomingMovieApiData(): Observable<any> { //new release movies watch in sri lanka
-  //   const today = new Date();
-  //   const oneWeekLater = new Date();
-  //   oneWeekLater.setDate(today.getDate() + 8)
-  //   const threeMonthsLater = new Date();
-  //   threeMonthsLater.setMonth(today.getMonth() + 3);
-
-  //     const payload = {
-  //       "airDateGte": null,
-  //       "airDateLte": null,
-  //       "certification": null,
-  //       "certificationCountry": "",
-  //       "debug": false,
-  //       "firstAirDateGte": null,
-  //       "firstAirDateLte": null,
-  //       "page": 2,
-  //       "primaryReleaseDateGte": oneWeekLater.toISOString().split('T')[0],
-  //       "primaryReleaseDateLte": threeMonthsLater.toISOString().split('T')[0],
-  //       "region": "US",
-  //       "releaseDateGte": "",
-  //       "releaseDateLte": "",
-  //       "showMe": 0,
-  //       "sortBy": "popularity",
-  //       "voteAverageGte": 0.0,
-  //       "voteAverageLte": 10.0,
-  //       "voteCountGte": 0,
-  //       "watchRegion": "LK",
-  //       "withGenres": "",
-  //       "withKeywords": null,
-  //       "withNetworks": null,
-  //       "withOriginCountry": null,
-  //       "withOriginalLanguage": "en",
-  //       "withWatchMonetizationTypes": null,
-  //       "withWatchProviders": null,
-  //       "withReleaseType": "",
-  //       "withRuntimeGte": 0,
-  //       "withRuntimeLte": 400
-  //   };
-
-  //   return this.http.post(`${this.baseurl}/tmdb/movies`,payload);
-  // }
-
-  // comingsoonMovieApiData(): Observable<any> { //new release movies watch in sri lanka
-  //   const today = new Date();
-  //   const oneWeekLater = new Date();
-  //   oneWeekLater.setDate(today.getDate() + 7);
-
-  //     const payload = {
-  //       "airDateGte": null,
-  //       "airDateLte": null,
-  //       "certification": null,
-  //       "certificationCountry": "",
-  //       "debug": false,
-  //       "firstAirDateGte": null,
-  //       "firstAirDateLte": null,
-  //       "page": 2,
-  //       "primaryReleaseDateGte": today.toISOString().split('T')[0],
-  //       "primaryReleaseDateLte": oneWeekLater.toISOString().split('T')[0],
-  //       "region": "US",
-  //       "releaseDateGte": "",
-  //       "releaseDateLte": "",
-  //       "showMe": 0,
-  //       "sortBy": "popularity",
-  //       "voteAverageGte": 0.0,
-  //       "voteAverageLte": 10.0,
-  //       "voteCountGte": 0,
-  //       "watchRegion": "LK",
-  //       "withGenres": "",
-  //       "withKeywords": null,
-  //       "withNetworks": null,
-  //       "withOriginCountry": null,
-  //       "withOriginalLanguage": "en",
-  //       "withWatchMonetizationTypes": null,
-  //       "withWatchProviders": null,
-  //       "withReleaseType": "",
-  //       "withRuntimeGte": 0,
-  //       "withRuntimeLte": 400
-  //   };
-
-  //   return this.http.post(`${this.baseurl}/tmdb/movies`,payload);
-  // }
-
   getSearchMovie(data: any): Observable<any> {
     // console.log(data,'movie#');
     return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apikey}&query=${data.movieName}`);
@@ -395,6 +269,22 @@ export class MovieApiServiceService {
         observe.complete();
       })
         .catch(e => errorCallback(e))
+    })
+  }
+
+  addToTrackList(_data: any, successCallback: Function, errorCallback: Function): Observable<any> {
+    return new Observable((observer) => {
+      axios.post(`${this.baseurl}/track-list`, _data, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('access_token')}`
+        }
+      })
+        .then(res => {
+          successCallback(res)
+          observer.next(res.data);
+          observer.complete();
+        }).catch(e => errorCallback(e))
     })
   }
 }
